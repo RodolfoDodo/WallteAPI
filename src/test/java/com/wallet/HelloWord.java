6cf69b54efb7c4a1fca16332bbc832529db58e1b
rodolfo farley <farleyrodolfo@gmail.com>
2021-02-16 01:21:42 -0200

    Configuração Inicial para Teste

--- src/test/java/com/wallet/HelloWord.java
+++ src/test/java/com/wallet/HelloWord.java
@@ -5,17 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
+public class HelloWord {
 
+	
 	@Test
-	public void contextLoads() {
+	public void testHelloWord() {
+		
 		assertEquals(1, 1);
 	}
-
 }
